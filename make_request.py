318cebe5f1b64661eb8b8bc920552e2db78b96ec
andreyJdev <andrey.jdev@gmail.com>
2024-09-11 19:35:32 +0300

    Pre-release

---
+++ make_request.py
@@ -0,0 +1,24 @@
+import requests
+import threading
+
+url = "http://localhost:8080/api/v1/wallet"
+data = {
+    "walletId": "427ec1f5-f229-4782-83ad-50007f4bc3c4",
+    "operationType": "DEPOSIT",
+    "amount": 1000
+}
+
+def send_requests():
+    for i in range(1000):
+        response = requests.post(url, json=data)
+        print(f"Request {i+1}: Status Code {response.status_code}")
+
+threads = []
+
+for _ in range(10):
+    thread = threading.Thread(target=send_requests)
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
